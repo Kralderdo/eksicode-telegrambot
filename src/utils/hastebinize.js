103ab4c0ce1ce325926c63ca0d0acb1056dcdb19
Atakan Ermiş <ataerms@gmail.com>
2021-06-15 16:22:00 +0300

    refactor hastebinize

--- src/utils/hastebinize.js
+++ src/utils/hastebinize.js
@@ -1,22 +1,20 @@
-const fetch = require('node-fetch');
+import fetch from 'node-fetch';
 
-// Will turn `text` into a hastebin document. 
+// Will turn `text` into a hastebin document.
 // Should return a Hastebin URL.
 
-async function hastebinize(text) { // Message.reply_to_message.text as argument.
+export default async (text) => {
   try {
-    const requestToHastebin = await fetch('https://hastebin.com/documents', { // POSTs message to hastebin. Should return an object
-      method: 'POST',                                                         // that contains hastebin document id.
-      body: text                                                              // Example: {key: 'ezuhusunaq'}
-    })
+    const requestToHastebin = await fetch('https://hastebin.com/documents', {
+      method: 'POST',
+      body: text,
+    });
 
-    const response = await requestToHastebin.json()
+    const response = await requestToHastebin.json();
 
-    return `https://hastebin.com/${response?.key}`
-
+    return `https://hastebin.com/${response?.key}`;
   } catch (err) {
-    return err
+    console.error(err);
+    return err;
   }
-}
-
-module.exports = hastebinize
+};
