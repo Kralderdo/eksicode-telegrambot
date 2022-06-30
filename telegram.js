fbc3e01c8bfb12705a698dc2fdcc9105bcbe7cdb
Mikail Tekin <mkltkn@gmail.com>
2022-06-30 02:57:43 +0200

    - crone added for users check

--- telegram.js
+++ telegram.js
@@ -1,7 +1,3 @@
-/**
- * Bu script, eksicode.org telegram grupları üzerinde ki kayıtlı üye sayılarını ölçmek
- * ve chat geliştirmeleri için yapılmıştır.
- */
 const path = require('path')
 require('dotenv').config({
   path: path.join(__dirname, '.env')
@@ -10,15 +6,28 @@ const { Telegraf } = require('telegraf')
 const bot = new Telegraf(process.env.BOT_TOKEN)
 const axios = require('axios')
 const { apiAuth } = require('./src/utils')
+const cron = require('node-cron')
 
 const sleep = (ms) => {
   return new Promise(resolve => setTimeout(resolve, ms))
 }
 
-(async () => {
+/**
+ * Cron Job
+ *
+ * Run crone job everyday
+ *
+ * @param '00 01 * * *' Evertyday will backup at 01
+ */
+
+cron.schedule('00 01 * * *', async () => {
+  // cron.schedule('1 * * * * *', async () => {
+  console.log('Eksicode User Counts Checking Started')
+  console.log('-----------------------------------------')
+
   const jwt = await apiAuth()
 
-  // Update chanel members 
+  // Update group members
   const updateMembers = async (id, members) => {
     try {
       const requestData = {
@@ -32,8 +41,8 @@ const sleep = (ms) => {
       }
       axios.put(`${process.env.API_URL}/telegrams/` + id, requestData, config)
         .catch(err => {
-          console.log(err);
-        });
+          console.log(err)
+        })
     } catch (err) {
       console.error(err)
     }
@@ -41,36 +50,40 @@ const sleep = (ms) => {
 
   let allGroups
 
-  // Get chanel list from api
+  // Get group list from api
   axios.get(`${process.env.API_URL}/telegrams?_sort=ListOrder:ASC`)
     .then(async function (response) {
       allGroups = response.data
     })
     .then(async () => {
-      console.log('toplam kanal sayısı: ' + allGroups.length)
+      console.log('total groups: ' + allGroups.length)
 
-      // Get chanel member count from Telegram 
+      // Get group member count from Telegram
       for (let i = 0; i < allGroups.length; i++) {
         await bot.telegram
           .getChatMembersCount(allGroups[i].channelID)
           .then(data => { allGroups[i].members = data })
           .then(async () => {
-            console.log(allGroups[i].name, allGroups[i].members + ' kişi')
+            console.log(allGroups[i].name, allGroups[i].members + ' user')
             await sleep(2000)
             updateMembers(allGroups[i].id, allGroups[i].members)
 
-            console.log(allGroups[i].name + ' Güncellendi...')
+            console.log(allGroups[i].name + ' Uptated...')
             console.log('-------------------------')
           })
           .catch(err => {
-            console.log(`${allGroups[i].name} için data çekilemedi.`)
-            console.log(err);
-          });
+            console.log(`${allGroups[i].name} wasn't respond any date.`)
+            console.log(err)
+          })
 
         delete allGroups[i].channelID
       }
     })
+    .then(() => {
+      console.log('Eksicode User Counts Checking finished')
+      console.log('-----------------------------------------')
+    })
     .catch((err) => {
       console.log(err)
     })
-})()
+})
